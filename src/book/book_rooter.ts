15af7e31b5cc2add613a04c05e926f70f9aff23e
Harshad N <103301901+harshad4321@users.noreply.github.com>
2022-07-02 23:40:09 +0530

    book create,add,update,delete, set is compleated

--- src/book/book_rooter.ts
+++ src/book/book_rooter.ts
@@ -3,6 +3,7 @@ import type{Request,Response}from "express";
 import { body ,validationResult} from "express-validator";
 
 import * as BookService from "./book_service";
+import { request } from "http";
 export const bookRouter = express.Router();
 
 
@@ -17,3 +18,70 @@ bookRouter.get("/",async(request:Request,response:Response)=>{
     }
 }) 
 
+//GET:A book based on the id
+bookRouter.get('/:id',async (request:Request,response:Response)=>{
+    const id:number = parseInt(request.params.id,10);
+    try{
+        const  book=await BookService.getBook(id)
+        if(book){
+            return response.status(200).json(book)
+        }
+    }catch(error:any){
+        return response.status(500).json(error.message)
+       }
+})
+
+bookRouter.post(
+    "/",
+body("title").isString(),
+body("authorId").isInt(),
+body("datePublished").isDate().toDate(),
+body("isFiction").isBoolean(),
+async(request:Request,response:Response)=>{
+    const error = validationResult(request);
+  if(!error.isEmpty())  {
+    return response.status(400).json({error:error.array()} ) ;
+  }
+  try{
+    const book = request.body
+    const newBook = await BookService.createBook(book)
+    return response.status(201).json(newBook)
+  }catch(error:any){
+    return response.status(500).json(error.message)
+   }
+
+}
+);
+//PUT: Update book
+bookRouter.put(
+    "/:id",
+body("title").isString(),
+body("authorId").isInt(),
+body("datePublished").isDate().toDate(),
+body("isFiction").isBoolean(),
+async(request:Request,response:Response)=>{
+    const error = validationResult(request);
+  if(!error.isEmpty())  {
+    return response.status(400).json({error:error.array()} ) ;
+  }
+  const id:number= parseInt(request.params.id,10)
+  try{ 
+    const book = request.body
+    const updatedBook = await BookService.updateBook(book,id)
+    return response.status(201).json(updatedBook)
+  }catch(error:any){
+    return response.status(500).json(error.message)
+   }
+
+}
+);
+
+bookRouter.delete("/:id",async(request:Request,response:Response)=>{
+    const id:number =parseInt(request.params.id,10);
+    try{
+        await BookService.deleteBook(id)
+        return response.status(204).json("Book was successfully deleted.")
+    }catch(error:any){
+        return response.status(500).json(error.message)
+       }
+})
