bfe2d88ac31a53490ffd3c5c000a84776b8a4494
Harshad N <103301901+harshad4321@users.noreply.github.com>
2022-07-02 16:56:56 +0530

    update is sat

--- src/author/author.service.ts
+++ src/author/author.service.ts
@@ -1,5 +1,6 @@
 //  import { Author } from "@prisma/client"
 import {db} from "../utils/db.server"
+import { authorRouter } from "./author.router";
 
 type Author={
     id:number;
@@ -21,11 +22,63 @@ type Author={
 
  };
 
+ //Get
+
  export const getAuthor =async (id:number):Promise<Author | null> =>{
  return db.author.findUnique({
     where:{
         id,
     },
- })
+    // select :{
+    //     id:true,
+    //     firstName:true,
+    //     lastName:true,
+    //     createdAt :true,
+    // },
+ });
     
- }
+ };
+// create
+ export const createAuthor = async(
+    author:Omit<Author, "id" >
+    ):Promise<Author>=>{
+ const{firstName,lastName}= author;
+  return db .author.create({
+ data:{
+    firstName,
+    lastName,
+ },
+ select :{
+    id:true,
+        firstName:true,
+        lastName:true,
+        createdAt :true,
+ },
+  });
+ };
+
+//update 
+ 
+  export const updateAuthor = async(author:Omit<Author,"id">, id :number):Promise<Author>=>{
+    const {firstName,lastName}=author;
+    return db.author.update({
+where:{
+    id,
+
+},
+data:{
+    firstName,
+    lastName
+},
+select:{
+    id:true,
+    firstName:true,
+    lastName:true,
+    createdAt :true,
+      },
+    });
+  };
+
+// delete
+
+//   export  const  delete
